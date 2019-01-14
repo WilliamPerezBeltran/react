f7468029d2a6c21fb98300276f58515085f2ac0f
williamperezbeltran <williampbeltranprogramador@gmail.com>
2019-01-14 15:14:35 -0500

    Props

---
+++ src/posts/PostList.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+import PostData from '../data/posts.json'
+import PostDetail from './PostDetail'
+class PostList extends Component {
+  render () {
+    return (
+      <div>
+        <h1>hello there</h1>
+        {PostData.map((item, index) => {
+          return <PostDetail post={item} key={`post-list-key ${index}`} />
+        })}
+      </div>
+    )
+  }
+}
+
+export default PostList
