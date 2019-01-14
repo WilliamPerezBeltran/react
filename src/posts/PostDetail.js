6e913be52bfaf82333cfbed88094e7b67c44bce4
williamperezbeltran <williampbeltranprogramador@gmail.com>
2019-01-14 15:29:19 -0500

    Click event

--- src/posts/PostDetail.js
+++ src/posts/PostDetail.js
@@ -1,12 +1,22 @@
 import React, { Component } from 'react'
 
 class PostDetail extends Component {
+  constructor (props) {
+    super(props)
+    this.titleWasClicked = this.titleWasClicked.bind(this)
+  }
+  titleWasClicked (event) {
+    event.preventDefault()
+    alert('click')
+    console.log(this.props)
+  }
+
   render () {
     const { post } = this.props
     // const { post } = this.props.post
     return (
       <div>
-        <h1>{post.title}</h1>
+        <h1 onClick={this.titleWasClicked}>{post.title}</h1>
         <p>{post.content}</p>
       </div>
     )
